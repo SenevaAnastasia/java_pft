722f371a255d1ab62cb3963b4e0f0277996286d6
SenevaAnastasia <nastya_27-1996@mail.ru>
2021-07-28 08:33:07 +0600

    Вычисление расстояния между двумя точками при помощи метода в классе Point (Задание 2.4)

--- sandbox/src/main/java/MyProgram.java
+++ sandbox/src/main/java/MyProgram.java
@@ -1,17 +1,9 @@
 public class MyProgram {
 	
 	public static void main (String[] args){
-		Point p1 = new Point();
-		Point p2 = new Point();
-		p1.a = 1;
-		p1.b = 5;
-		p2.a = 2;
-		p2.b = 5;
-		System.out.println("Расстояние между точкой p1(" + p1.a + ";" + p1.b + ") и точкой p2(" + p2.a + ";" + p2.b + ") = " + distance(p1, p2));
-	}
-
-	public static double distance(Point p1, Point p2){
-		return Math.sqrt((p2.a - p1.a)*(p2.a - p1.a) + (p2.b - p1.b)*(p2.b - p1.b));
+		Point p1 = new Point(1, 5);
+		Point p2 = new Point(1, 10);
+		System.out.println("Расстояние между точкой p1(" + p1.a + ";" + p1.b + ") и точкой p2(" + p2.a + ";" + p2.b + ") = " + p1.distance(p2));
 	}
 
 }
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -2,4 +2,20 @@ public class Point {
 
     public double a;
     public double b;
+
+    public Point(double a, double b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public double distance(double a, double b){
+        double da = this.a - a;
+        double db = this.b - b;
+        return Math.sqrt(da*da + db*db);
+    }
+
+    public double distance(Point p)
+    {
+        return distance(p.a, p.b);
+    }
 }
